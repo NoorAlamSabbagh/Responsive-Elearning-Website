e5fb104c303c6337891cf74de0c9a1d553d9911b
Noor Alam <99783083+NoorAlamSabbagh@users.noreply.github.com>
2023-10-03 22:38:41 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -9,5 +9,4 @@ burger.addEventListener('click', () => {
     rightNav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
-
 })
